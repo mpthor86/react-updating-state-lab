7568091b9abd6d7dcb0ccb20c02a984fb2744500
michael <mpthornton86@gmail.com>
2021-02-26 17:35:11 -0700

    commit tree 0b924e1a408bd21e5e1eda4f2128c9baf7f2de47

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+    state = {timesClicked: 0}
+
+    updateState = () => {
+        this.setState(prevState => {
+            return {timesClicked: prevState.timesClicked + 1}
+        })
+    }
+
+    render() {
+        return(
+            <button label={this.state.timesClicked} onClick={this.updateState}>{this.state.timesClicked}</button>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,44 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      } 
+
+      updateBitrate = () => {
+          this.setState({
+              settings: {
+                  ...this.state.settings,
+                  bitrate: 12
+              }
+          })
+      }
+
+      updateRes = () => {
+          this.setState({
+              settings: { 
+                  ...this.state.settings,
+                    video: {
+                        ...this.state.settings.video,
+                        resolution: '720p'
+                    }}
+          })
+      }
+
+    render() {
+        return (
+            <div>
+            <button className="bitrate" onClick={this.updateBitrate}>BitRate</button>
+            <button className='resolution' onClick={this.updateRes}>Res</button>
+            </div>
+        )
+    }
+}
